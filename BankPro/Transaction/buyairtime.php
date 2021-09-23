3c4c5cafc7cf0ee72654d4cd645c6a539e663c45
Mayowasamuel51 <87167237+Mayowasamuel51@users.noreply.github.com>
2021-09-23 03:32:13 +0100

    Add files via upload

---
+++ BankPro/Transaction/buyairtime.php
@@ -0,0 +1,99 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=`, initial-scale=1">
+	 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+	<title>Login </title>
+</head>
+<body>
+
+<style>
+	body{
+		background-image: linear-gradient(to right top, #4a494f, #5c4c6a, #7c497b, #a33d7e, #cb2474);
+	}
+	.tops{
+			margin-top: 120px;
+		}
+</style></body>
+
+
+
+<form action="">
+	
+
+
+<div class="columns">
+		<div class="tops column is-offset-4">
+			
+			<label for="" class="is-size-3 has-text-white">Phone  Number</label><br>
+			<input type="text" class="input is-primary  column is-5" placeholder="Phone Number"><br>
+
+
+			<label for="" class="is-size-3 has-text-white">Amount  </label><br>
+			<input type="text" class="input is-primary column is-5" placeholder="Amount"><br>
+
+
+			<label for="" class="is-size-3 has-text-white">Pin Number </label><br>
+			<input type="text" class="input is-primary column is-5" placeholder="Pin Number"><br>
+
+
+
+
+
+			<div class="mt-5">
+				<button class="button is-info px-6  mr-3" type="submit" name="sub">Buy </button>
+			
+			</div>
+
+
+
+
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
